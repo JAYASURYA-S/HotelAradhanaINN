1c984253962e3fee4737393600f23043a27ad2c3
JAYASURYA-S <jayasurya5858@gmail.com>
2023-11-13 16:21:23 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
+    <Navbar/>
     <button className='btn btn-primary'>hello</button>
     </>
   );
